11d555d0624394a94018e8c1ca8640e18724f497
MarcosDMT <marcosgav80@gmail.com>
2023-08-08 14:37:13 +0300

    polished the sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@
 import classNames from "classnames";
 import React, { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
+import Sidebar from "./Sidebar";
 const Layout = ({children}) => {
   const [collapsed, setSidebarCollapsed] = useState(false);
   return (
@@ -17,11 +18,15 @@ const Layout = ({children}) => {
       })}
     >
       {/* sidebar */}
-      <div className="bg-indigo-700 text-white">
+      <Sidebar 
+       collapsed={collapsed}
+       setCollapsed={()=> setSidebarCollapsed((prev) => !prev)}
+      />
+      {/* <div className="bg-indigo-700 text-white">
         <button onClick={() => setSidebarCollapsed((prev) => !prev)}>
           <MenuIcon className="w-10 h-10" />
         </button>
-      </div>
+      </div> */}
       {/* content */}
       <div className=""> {children}</div>
     </div>
